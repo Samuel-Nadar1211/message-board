ee86b33ffbe721300b9e244acae478159913d97d
Samuel <samuel.nadar1211@gmail.com>
2022-07-15 19:50:50 +0530

    initial commit

---
+++ posts/models.py
@@ -0,0 +1,9 @@
+# Create your models here.
+from django.db import models
+
+
+class Post(models.Model):  # new
+    text = models.TextField()
+
+    def __str__(self):  # new
+        return self.text[:50]
